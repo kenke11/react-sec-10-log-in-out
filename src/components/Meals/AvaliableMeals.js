681e5c9fdd6889fec0a96c733a38a005d5ca7f18
kenke11 <kenkebashvili2@gmail.com>
2022-01-25 15:45:06 +0400

    react: adding http & forms to the food order app - section 17

--- src/components/Meals/AvaliableMeals.js
+++ src/components/Meals/AvaliableMeals.js
@@ -1,44 +1,58 @@
 import classes from './AvaliableMeals.module.css'
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import {useEffect, useState} from "react";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
 
 const AvaliableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(
-        meal => <MealItem
-            key={meal.id}
-            id={meal.id}
-            name={meal.name}
-            description={meal.description}
-            price={meal.price}
-        />
-    )
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        fetch('https://react-food-http-c4a6f-default-rtdb.firebaseio.com/meals.json')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setMeals(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error)
+                }
+            )
+    }, [])
+
+    const loadedMeals = [];
+
+    for (const key in meals) {
+        loadedMeals.push({
+            id: key,
+            description: meals[key].description,
+            name: meals[key].name,
+            price: meals[key].price
+        })
+    }
+
+    let mealsList;
+
+    if (error) {
+        mealsList = <p className="font-bold">Not fount</p>
+    } else if (!isLoaded) {
+        mealsList = <p>Loading...</p>
+    } else {
+        mealsList = loadedMeals.map(meal => (
+            <MealItem
+                key={meal.id}
+                id={meal.id}
+                name={meal.name}
+                description={meal.description}
+                price={meal.price}
+            />
+        ))
+    }
+
 
     return (
         <section className={classes.meals}>
